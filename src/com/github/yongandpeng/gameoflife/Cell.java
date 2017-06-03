0b813c81aac2d55050c7b817abe9b61d5eb85ba5
masidaxiaoming <1040412498@qq.com>
2017-06-03 12:06:23 +0800

    单个点的测试
    单个点的测试

--- src/com/github/yongandpeng/gameoflife/Cell.java
+++ src/com/github/yongandpeng/gameoflife/Cell.java
@@ -5,10 +5,48 @@ package com.github.yongandpeng.gameoflife;
  */
 public class Cell {
     public static void nextState(int[][] matrix, int x, int y, int n) {
-
+        int liveCell = 0;
+        if (validateArrayOutofBounds(x-1, y, n) && matrix[x-1][y] == 1 ) {
+            liveCell++;
+        }
+        if (validateArrayOutofBounds(x+1, y, n) && matrix[x+1][y] == 1 ){
+            liveCell++;
+        }
+        if (validateArrayOutofBounds(x-1, y-1, n)  && matrix[x-1][y-1] == 1 ){
+            liveCell++;
+        }
+        if (validateArrayOutofBounds(x, y-1, n)  && matrix[x][y-1] == 1 ){
+            liveCell++;
+        }
+        if (validateArrayOutofBounds(x+1, y-1, n)  && matrix[x+1][y-1] == 1 ){
+            liveCell++;
+        }
+        if (validateArrayOutofBounds(x-1, y+1, n)  && matrix[x-1][y+1] == 1 ){
+            liveCell++;
+        }
+        if (validateArrayOutofBounds(x, y+1, n)  && matrix[x][y+1] == 1 ){
+            liveCell++;
+        }
+        if (validateArrayOutofBounds(x+1, y+1, n)  && matrix[x+1][y+1] == 1 ){
+            liveCell++;
+        }
+        if(liveCell >= 3){
+            matrix[x][y] = 1;
+        }else if(liveCell < 2){
+            matrix[x][y] = 0;
+        }
     }
 
     public static void main(String[] args) {
 
     }
+
+    private static boolean validateArrayOutofBounds(int x,int y,int n){
+        boolean flag = false;
+        if(x >= 0 && y >= 0 && x < n && y < n){
+            flag = true;
+        }
+        return  flag;
+    }
+
 }
