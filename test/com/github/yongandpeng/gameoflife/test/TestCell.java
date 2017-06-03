e3116cc2f46523200b58c9db7bfff38d5a5e282c
bboxhe <bboxhe@gmail.com>
2017-06-03 11:45:48 +0800

    Update test

--- test/com/github/yongandpeng/gameoflife/test/TestCell.java
+++ test/com/github/yongandpeng/gameoflife/test/TestCell.java
@@ -12,8 +12,63 @@ public class TestCell {
 
     @Test
     public void shouldReturnNextState() {
+        int x, y, v, n;
+        n = 3;
         int[][] matrix= {{0, 0, 0}, {1, 1, 1}, {0, 0, 0}};
-        Cell.nextState(matrix, 0, 0, 3);
-        assertEquals(matrix[0][0], 0);
+
+        x = 0;
+        y = 0;
+        v = 0;
+
+        Cell.nextState(matrix, x, y, n);
+        assertEquals(matrix[x][y], v);
+
+        x = 1;
+        y = 0;
+        v = 1;
+        Cell.nextState(matrix, x, y, n);
+        assertEquals(matrix[x][y], v);
+
+        x = 2;
+        y = 0;
+        v = 0;
+        Cell.nextState(matrix, x, y, n);
+        assertEquals(matrix[x][y], v);
+
+        x = 0;
+        y = 1;
+        v = 0;
+        Cell.nextState(matrix, x, y, n);
+        assertEquals(matrix[x][y], v);
+
+        x = 1;
+        y = 1;
+        v = 1;
+        Cell.nextState(matrix, x, y, n);
+        assertEquals(matrix[x][y], v);
+
+        x = 2;
+        y = 1;
+        v = 0;
+        Cell.nextState(matrix, x, y, n);
+        assertEquals(matrix[x][y], v);
+
+        x = 0;
+        y = 2;
+        v = 0;
+        Cell.nextState(matrix, x, y, n);
+        assertEquals(matrix[x][y], v);
+
+        x = 1;
+        y = 2;
+        v = 0;
+        Cell.nextState(matrix, x, y, n);
+        assertEquals(matrix[x][y], v);
+
+        x = 2;
+        y = 2;
+        v = 0;
+        Cell.nextState(matrix, x, y, n);
+        assertEquals(matrix[x][y], v);
     }
 }
